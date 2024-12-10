51cd0be15d6f538c524ac2649ded405945d68956
LaGavroche <rayan.boiteau@gmail.com>
2024-12-10 16:11:58 +0100

    constructeur

--- Controllers/EtudiantController.cs
+++ Controllers/EtudiantController.cs
@@ -1,16 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
-
 using mvc.Models;
-
+using mvc.Data;
 namespace mvc.Controllers;
+
+
+
 public class EtudiantController : Controller {
+    private readonly ApplicationDbContext _context;
      private static List<Etudiant> _etudiants = new List<Etudiant>
     {
         new Etudiant { Id = 1, Nom = "Alice", Age = 20},
         new Etudiant { Id = 2, Nom = "Bob", Age = 22},
         new Etudiant { Id = 3, Nom = "Charlie", Age = 21}
     };
-public IActionResult Index()
+       // Constructeur
+    public EtudiantController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult Index()
     {
         return View(_etudiants);
     }
